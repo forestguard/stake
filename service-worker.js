4e713209994386f1ab79223677918420f577a557
theomnipotentbeing <72243555+theomnipotentbeing@users.noreply.github.com>
2020-10-22 18:05:35 +0700

    Update Pages.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e50f7e8ac70f18913254ca07df04efc4.js"
+  "/precache-manifest.fcbb55123578c22dc47694904ec76c19.js"
 );
 
 self.addEventListener('message', (event) => {
